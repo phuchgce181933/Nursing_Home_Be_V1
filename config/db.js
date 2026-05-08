7dcbd4cb1ddfec1d5590ecaf1863713e4f0b2140
phuchgce181933 <phuchgce181933@gmail.com>
2026-05-08 21:27:40 +0700

    db_local

--- config/db.js
+++ config/db.js
@@ -1,10 +1,21 @@
+
+// module.exports = connectDB;
 require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+
+    // chọn local hoặc atlas
+    const uri =
+      process.env.NODE_ENV === 'local'
+        ? process.env.MONGO_URI_LOCAL
+        : process.env.MONGO_URI;
+
+    await mongoose.connect(uri);
+
+    console.log('MongoDB connected:', uri);
+
   } catch (err) {
     console.error(err);
     process.exit(1);
@@ -12,3 +23,5 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+// sử dụng lệnh này để chạy local: npm run dev:local
+// sử dụng lệnh này để chạy atlas: npm run dev

225b3b6bb5a7dfc01a30c07788ef34dbb5d0b712
PhucHG <Phuchgce181933@fpt.edu.vn>
2026-06-08 14:07:25 +0700

    payment_service

--- routes/payos.js
+++ routes/payos.js
@@ -4,5 +4,7 @@ const payosController = require('../controllers/payosController');
 const router = express.Router();
 
 router.post('/webhook', payosController.handleWebhook);
+router.get('/return', payosController.handleReturn);
+router.get('/cancel', payosController.handleCancel);
 
 module.exports = router;
